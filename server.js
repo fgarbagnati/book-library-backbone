27e27d6aa777d3093e2abeea0eda1912030f1573
fgarbagnati <f.g.garbagnati@gmail.com>
2014-09-05 12:58:58 -0700

    update route works

--- server.js
+++ server.js
@@ -85,3 +85,22 @@ app.get( '/api/books/:id', function( request, response ) {
     }
   });
 });
+
+// Update a book
+app.put( '/api/books/:id', function( request, response ) {
+  console.log( 'Updating book ' + request.body.title );
+  return BookModel.findById( request.params.id, function( err, book ) {
+    book.title = request.body.title;
+    book.author = request.body.author;
+    book.releaseDate = request.body.releaseDate;
+
+    return book.save( function( err ) {
+      if( !err ) {
+        console.log( 'book updated' );
+        return response.send( book );
+      } else {
+        console.log( err );
+      }
+    });
+  });
+});
