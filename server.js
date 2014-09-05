ef0f53a69049c45842593f92fa91cb3de985b2bd
fgarbagnati <f.g.garbagnati@gmail.com>
2014-09-05 13:04:43 -0700

    deleting books works

--- server.js
+++ server.js
@@ -104,3 +104,18 @@ app.put( '/api/books/:id', function( request, response ) {
     });
   });
 });
+
+// Delete a book
+app.delete( '/api/books/:id', function( request, response ) {
+  console.log( 'Deleting book with id: ' + request.params.id );
+  return BookModel.findById( request.params.id, function( err, book ) {
+    return book.remove( function( err ) {
+      if( !err ) {
+        console.log( 'book removed' );
+        return response.send( '' );
+      } else {
+        console.log( err );
+      }
+    });
+  });
+});
